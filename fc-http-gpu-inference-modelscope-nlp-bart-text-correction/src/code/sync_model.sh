e915c5138c2fd7910acf20ea418d30f3dba478e9
robinsonnie <robinsonnie@gmail.com>
2023-02-01 11:33:23 +0800

    improve sync model script

--- fc-http-gpu-inference-modelscope-nlp-bart-text-correction/src/code/sync_model.sh
+++ fc-http-gpu-inference-modelscope-nlp-bart-text-correction/src/code/sync_model.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 wget "https://fc-start-gpu-code-samples-hz.oss-cn-hangzhou.aliyuncs.com/fc-http-gpu-inference-modelscope-nlp-bart-text-correction/model.tar"
 tar xvf model.tar
+rm -rf model.tar
